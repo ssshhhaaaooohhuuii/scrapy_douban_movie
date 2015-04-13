0a6d0d9e0465612c2c4f58b0d65751a93b61a1ca
kaito <silverbulletkaito@gmail.com>
2015-04-13 23:51:23 +0800

    init repo

---
+++ scrapy_douban_movie/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for scrapy_douban_movie project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'scrapy_douban_movie'
+
+SPIDER_MODULES = ['scrapy_douban_movie.spiders']
+NEWSPIDER_MODULE = 'scrapy_douban_movie.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'scrapy_douban_movie (+http://www.yourdomain.com)'
